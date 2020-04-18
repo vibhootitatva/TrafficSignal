d16938bab094e8857caa758ff9e9a3f1985b8a5d
Saurabh Rajput <saurabh.rajput@tatvasoft.com>
2020-04-18 11:43:44 +0530

    initial commit

---
+++ TrafficSignal/TrafficSignal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrafficSignal
+//
+//  Created by PCQ184 on 18/04/20.
+//  Copyright © 2020 PCQ184. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
