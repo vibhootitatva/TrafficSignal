75f55f687da636617c0d1b39bcfd131e700856be
Saurabh Rajput <saurabh.rajput@tatvasoft.com>
2020-04-18 13:15:54 +0530

    adding functionality in viewcontroller

--- TrafficSignal/TrafficSignal/ViewController.swift
+++ TrafficSignal/TrafficSignal/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var sideALabel: UILabel!
+    @IBOutlet weak var sideBLabel: UILabel!
+    @IBOutlet weak var sideCLabel: UILabel!
+    @IBOutlet weak var sideDLabel: UILabel!
+    @IBOutlet weak var sideAButton: UIButton!
+    @IBOutlet weak var sideBButton: UIButton!
+    @IBOutlet weak var sideCButton: UIButton!
+    @IBOutlet weak var sideDButton: UILabel!
+    var counter = 10
     override func viewDidLoad() {
         super.viewDidLoad()
+        let timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(signal), userInfo: nil, repeats: true)
         // Do any additional setup after loading the view.
     }
-
-
+    @objc func signal() {
+        if(counter > 0) {
+            counter = counter - 1
+            sideALabel.text = "\(counter)"
+        }
+        
+    }
 }
-
